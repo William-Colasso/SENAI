d4fa6cd7bb7b1fd5f037ef8291aa628ef39ac7ad
William Colasso <161973026+Lilam0@users.noreply.github.com>
2024-05-15 13:31:50 -0300

    .

---
+++ Aprendizagem em Programador de sistemas/Lógica de Programação/Projeto grupo 8/src/projeto/grupo/pkg8/ProjetoGrupo8.java
@@ -0,0 +1,14 @@
+package projeto.grupo.pkg8;
+import java.util.Scanner;
+
+public class ProjetoGrupo8 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite algo para saber a quantidade de caracteres");
+        String frase = scanner.nextLine();
+        System.out.println("A quantidade de caracteres é: " + frase.length());
+        
+    }
+    
+}
