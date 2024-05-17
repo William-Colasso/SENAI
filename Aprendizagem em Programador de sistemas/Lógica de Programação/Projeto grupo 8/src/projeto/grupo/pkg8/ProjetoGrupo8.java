381d6bd310127b4846195f69c0aa68cb8cdbbf9c
William Colasso <161973026+William-Colasso@users.noreply.github.com>
2024-05-17 13:58:18 -0300

    Update ProjetoGrupo8.java

--- Aprendizagem em Programador de sistemas/Lógica de Programação/Projeto grupo 8/src/projeto/grupo/pkg8/ProjetoGrupo8.java
+++ Aprendizagem em Programador de sistemas/Lógica de Programação/Projeto grupo 8/src/projeto/grupo/pkg8/ProjetoGrupo8.java
@@ -1,14 +1,33 @@
 package projeto.grupo.pkg8;
-import java.util.Scanner;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.Scanner;
+//Contador de caracteres em JAVA, William Colasso
 public class ProjetoGrupo8 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite algo para saber a quantidade de caracteres");
         String frase = scanner.nextLine();
+        clearConsole();
         System.out.println("A quantidade de caracteres é: " + frase.length());
-        
+
     }
-    
+
+    //Limpa Console
+    public final static void clearConsole() {
+        //Função feita por Gabriel Piske
+        try {
+            Robot robot = new Robot();
+            robot.setAutoDelay(10);
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_L);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyRelease(KeyEvent.VK_L);
+        } catch (AWTException ex) {
+        }
+    }
+
 }
