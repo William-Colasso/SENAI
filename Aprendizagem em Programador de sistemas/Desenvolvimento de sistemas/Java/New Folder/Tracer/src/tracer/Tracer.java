3bcc3c1e0bb278cd55bf4cbc79e915b60b0673a0
William Colasso <william.colasso@gmail.com>
2024-07-27 21:29:22 -0300

    Novo projeto iniciado
    Novo projeto: Refazendo o traçador gráficos em versão Java

---
+++ Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Java/New Folder/Tracer/src/tracer/Tracer.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tracer;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Tracer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame form = new JFrame();
+       form.setTitle("Graphic Tracer");
+       form.setSize(1920, 1080);
+       form.setLocationRelativeTo(null);
+       form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       form.setVisible(true);
+    }
+    
+}
---
+++ Aprendizagem em Programador de sistemas/Lógica de Programação/Java/Projeto grupo 8/GraphicTracer/src/graphictracer/GraphicTracer.java
@@ -0,0 +1,24 @@
+
+package graphictracer;
+
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @William-Colasso
+ */
+
+public class GraphicTracer {
+
+    public static void graphictracer(String[] args) {
+       JFrame form = new JFrame();
+       form.setTitle("Graphic Tracer");
+       form.setSize(1920, 1080);
+       form.setLocationRelativeTo(null);
+       form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       form.setVisible(true);
+    }
+    
+}
