f8277b56e43d6f0d65595e0324f27a0c4e080683
William Colasso <161973026+William-Colasso@users.noreply.github.com>
2024-08-29 16:44:20 -0300

    FINALMENTE FOI A TABELA

--- Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Introdução a WEB/App WEB/WebCad/www/script.js
+++ Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Introdução a WEB/App WEB/WebCad/www/script.js
@@ -1,35 +1,38 @@
-document.getElementById(`clearButton`).addEventListener(`click`, cleam);
-document.getElementById(`addButton`).addEventListener(`click`, send);
+document.getElementById('clearButton').addEventListener('click', cleam);
+document.getElementById('addButton').addEventListener('click', send);
 var actualCell = 0;
 
 function cleam() {
-  const inputs = document.querySelectorAll(".forms");
+  const inputs = document.querySelectorAll('.forms');
 
   inputs.forEach((input) => {
-    input.value = "";
+    input.value = '';
   });
 }
+
 function send() {
-  const inputs = document.querySelectorAll(".forms");
-  continues = true;
-  inputs.forEach((input) => {
-    if (continues) {
-      if (input.value == "") {
-        alert("Existem parametros vazios!");
-        continues = false;
-        cleam();
-      } else {
-        const table = document.getElementById("table");
-        const newRow = document.createElement("tr");
+  const inputs = document.querySelectorAll('.forms');
+  let continues = true;
+  if (inputs.length > 0) {
+    var table = document.getElementById('table');
+    var newRow = document.createElement('tr'); // Cria uma nova linha
 
-        for (let i = 0; i < 6; i++) {
-          var auxi = document.createElement("td");
-          auxi.innerText = String(input.value);
-          newRow.appendChild(auxi);
+    inputs.forEach((input) => {
+      if (continues) {
+        if (input.value == '') {
+          alert('Existem parâmetros vazios!');
+          continues = false;
+          cleam();
+        } else {
+          var auxi = document.createElement('td'); // Cria uma nova célula
+          auxi.innerText = String(input.value); // Define o texto da célula como o valor do input
+          newRow.appendChild(auxi); // Adiciona a célula à linha
         }
-
-        table.appendChild(newRow);
       }
+    });
+
+    if (continues) {
+      table.appendChild(newRow); // Adiciona a nova linha à tabela se todos os inputs forem válidos
     }
-  });
+  }
 }
