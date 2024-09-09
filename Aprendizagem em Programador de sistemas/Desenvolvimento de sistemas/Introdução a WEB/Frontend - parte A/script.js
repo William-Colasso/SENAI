d49bf8a3f9c2908463db9b4187374767ec15bed8
William Colasso <161973026+William-Colasso@users.noreply.github.com>
2024-09-09 16:29:23 -0300

    Exercício finalizado

--- Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Introdução a WEB/Frontend - parte A/script.js
+++ Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Introdução a WEB/Frontend - parte A/script.js
@@ -8,7 +8,6 @@ function exibirSelecionados() {
   var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
   var checkValues = [];
   checkboxes.forEach((checkbox) => {
-   
     checkValues.push(checkbox.value);
   });
 
@@ -24,36 +23,49 @@ function exibirSelecionados() {
 
   textInputResult.value = inputText.value;
   textAreaResult.textContent = textArea.value;
-  checkBoxResult.value = "Checkboxes selecionados: "+checkValues;
-  radioResult.value = "Radio selecionado: "+radioValue;
+  checkBoxResult.value =
+    "Checkboxes selecionados: " +
+    (checkValues.length > 0 ? checkValues.join(",") : "Nenhuma");
+  radioResult.value = "Radio selecionado: " + radioValue;
   inputText.value = "";
-  textArea.value = "";  
-  
+  textArea.value = "";
+
+  radioButtons.forEach((radio) => {
+    radio.checked = false;
+  });
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  });
 }
 
-const buttonAddTable = document.getElementById('addTable')
-buttonAddTable.addEventListener('click', addTable)
-function addTable(){
-    
-    const tbody = document.querySelector('tbody')
-    const trow = document.createElement('tr')
+const buttonAddTable = document.getElementById("addTable");
+buttonAddTable.addEventListener("click", addTable);
+function addTable() {
+  const tbody = document.querySelector("tbody");
+  const trow = document.createElement("tr");
 
-    const c1 = document.createElement('td')
-    const nome = document.getElementById('nome')
-    c1.innerText = nome.value
-    nome.value = ""
-    const c2 = document.createElement('td')
-    const idade = document.getElementById('idade')
-    c2.innerText = idade.value
-    idade.value = ""
-    const c3 = document.createElement('td')
-    const email = document.getElementById('email')
-    c3.innerText = email.value
-    email.value = ""
+  const c1 = document.createElement("td");
+  const nome = document.getElementById("nome");
+  c1.innerText = nome.value;
+  
+  const c2 = document.createElement("td");
+  const idade = document.getElementById("idade");
+  c2.innerText = idade.value;
+  
+  const c3 = document.createElement("td");
+  const email = document.getElementById("email");
+  c3.innerText = email.value;
+  
 
-
-    trow.appendChild(c1)
-    trow.appendChild(c2)
-    trow.appendChild(c3)
-    tbody.append(trow)
+  if (email.value != "" && idade.value != "" && nome.value != "") {
+    trow.appendChild(c1);
+    trow.appendChild(c2);
+    trow.appendChild(c3);
+    tbody.append(trow);
+    nome.value = "";
+idade.value = "";
+email.value = "";
+  }else{
+    alert("Faltam informações para o envio para a tabela")
+  }
 }
