23abf0fbe023c70faea67d08be454c0e981fda21
William Colasso <161973026+William-Colasso@users.noreply.github.com>
2024-09-18 16:06:12 -0300

    Nova Classe para o Cliente

---
+++ Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Java/AppCadastroDesign/src/Cliente.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author william_c_pereira
+ */
+public class Cliente {
+
+    private int codigo;
+    private String nome;
+    private String fone;
+    private String email;
+    private String endereco;
+
+    
+    
+    //Construtores
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String fone) {
+
+        this.nome = nome;
+        this.fone = fone;
+
+    }
+    
+    //Getters
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+    
+    //Setters
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+    
+    
+
+}
