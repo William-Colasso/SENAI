ceae401b3f4cd802ff85666045cde93995a61b87
William Colasso <161973026+William-Colasso@users.noreply.github.com>
2024-10-09 14:12:54 -0300

    Initial commit

---
+++ Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Spring/appproduto/src/main/java/com/PSII/appproduto/AppprodutoApplication.java
@@ -0,0 +1,13 @@
+package com.PSII.appproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppprodutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppprodutoApplication.class, args);
+	}
+
+}
---
+++ Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Spring/appproduto/src/test/java/com/PSII/appproduto/AppprodutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PSII.appproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppprodutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
