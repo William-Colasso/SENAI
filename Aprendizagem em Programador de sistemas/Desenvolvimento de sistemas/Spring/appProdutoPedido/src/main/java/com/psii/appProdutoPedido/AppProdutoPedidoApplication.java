fadba65f96365bde9bc9899b86e8ebed1471ae63
WILLIAM COLASSO PEREIRA <william_c_pereira@estudante.sesisenai.org.br>
2024-10-16 15:37:51 -0300

    New exec

---
+++ Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Spring/appProdutoPedido/src/main/java/com/psii/appProdutoPedido/AppProdutoPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.psii.appProdutoPedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppProdutoPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppProdutoPedidoApplication.class, args);
+	}
+
+}
---
+++ Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Spring/appProdutoPedido/src/test/java/com/psii/appProdutoPedido/AppProdutoPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psii.appProdutoPedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProdutoPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
