05e6f9cc0f4162f6f76c00ac6db8d97947388bf0
WILLIAM COLASSO PEREIRA <william_c_pereira@estudante.sesisenai.org.br>
2024-10-21 14:17:59 -0300

    novo exercicio

---
+++ Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Spring/app_cad_pro/src/main/java/com/psii/app_cad_pro/AppCadProApplication.java
@@ -0,0 +1,13 @@
+package com.psii.app_cad_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCadProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCadProApplication.class, args);
+	}
+
+}
---
+++ Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Spring/app_cad_pro/src/test/java/com/psii/app_cad_pro/AppCadProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psii.app_cad_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCadProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
