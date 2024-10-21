dc90aa763fdafaff5edb5a37cca3f4bd5ac4f1e0
WILLIAM COLASSO PEREIRA <william_c_pereira@estudante.sesisenai.org.br>
2024-10-21 16:49:55 -0300

    Primeira parte completa

---
+++ Aprendizagem em Programador de sistemas/Desenvolvimento de sistemas/Spring/app_cad_pro/src/main/java/com/psii/app_cad_pro/model/Produto.java
@@ -0,0 +1,112 @@
+package com.psii.app_cad_pro.model;
+
+import jakarta.persistence.Transient;
+import java.io.IOException;
+import org.springframework.web.multipart.MultipartFile;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+    private Integer quantidade;
+    private String categoria;
+    private String fornecedor;
+
+    @Transient
+    private MultipartFile imagem;
+
+    @Lob
+    @Column(name = "imagem")
+    private byte[] imagemBytes;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public MultipartFile getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(MultipartFile imagem) {
+        this.imagem = imagem;
+        try {
+            this.imagemBytes = imagem.getBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] getImagemBytes() {
+        return imagemBytes;
+    }
+
+    public void setImagemBytes(byte[] imagemBytes) {
+        this.imagemBytes = imagemBytes;
+    }
+
+}
